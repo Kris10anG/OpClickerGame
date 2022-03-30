272f6222e77476ec9679420911392c5b2141fc9b
Kris10anG <kristiang@getacademy.no>
2022-03-30 12:25:17 +0200

    Add project files.

---
+++ OpClickerGame/Clicker.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpClickerGame
+{
+    internal class Clicker
+    {
+        public int PointsPerClick = 1;
+        public char KeyPress = 's';
+        public string Press {get; set;}
+        public int Count = 0;
+        public int TotalPoints = 0;
+
+        public void ClickForPoints()
+        {
+            Console.Clear();
+            TotalPoints += PointsPerClick;
+                //Console.ReadKey();
+                Console.WriteLine("Total Points: " + TotalPoints);
+              
+
+        }
+
+        public void Upgrade1()
+        {
+            if (TotalPoints >= 10)
+            {
+                TotalPoints -= 10;
+                PointsPerClick++;
+                Console.WriteLine("Bought Upgrade 1 for 10 Points. +1 PPC");
+                Console.WriteLine($"Total Points: {TotalPoints}");
+            }
+            else
+            {
+                Console.WriteLine("Du har ikke nok poeng til oppgradering!");
+            }
+        }
+
+    }
+}
---
+++ OpClickerGame/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace OpClickerGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var clicker = new Clicker();
+            Console.WriteLine("WELCOME TO THA OP CLICKA GAME!!!");
+
+            while (true)
+            {
+                var command = Console.ReadKey(true);
+                if (command.KeyChar == '1')
+                {
+                    clicker.ClickForPoints();
+                }
+                else if (command.KeyChar == '2')
+                {
+                    clicker.Upgrade1();
+                }
+            }
+        }
+    }
+}
+/*
+ - Poeng per klikk
+ - Oppgradering for å få flere poeng per klikk?
+ - Kan vinne klikkerspillet?
+ - 
+*/
